5f8be439134361bc5d1c8964500f29f3dca0fe46
Sam Pettersson <sam@izettle.com>
2017-01-08 19:44:42 +0100

    Add page publishing

--- pages/common/components/PageEditor/components/VersionList.js
+++ pages/common/components/PageEditor/components/VersionList.js
@@ -3,9 +3,25 @@ import styled from "styled-components"
 
 import { dispatch } from "../../../event"
 
+import putPage from "../../../put/pages"
+
 const ListItem = styled.li`
   backgroundColor: ${props => props.selected && "red"}
 `
+
+const unpublishAll = page =>
+  page.versions.forEach(version => { version.published = false })
+
+const publishVersion = props => version => {
+  const page = props.page
+  const nextState = !version.published
+
+  unpublishAll(page)
+
+  version.published = nextState
+
+  putPage(page).then(() => dispatch("PageVersionUpdated"))
+}
 
 export default props =>
   <ul>
@@ -15,6 +31,10 @@ export default props =>
         key={version._id}>
         <button onClick={() => { dispatch("ChangeVersion", version._id) }}>
           {version.created}
+        </button>
+
+        <button onClick={() => publishVersion(props)(version)}>
+          {version.published ? "Unpublish" : "Publish" }
         </button>
       </ListItem>
     )}
